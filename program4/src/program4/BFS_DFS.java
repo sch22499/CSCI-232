6a610b0585bb87d93453c679aec312d0dc439b34
chocogizmo <34075505+chocogizmo@users.noreply.github.com>
2018-06-06 07:43:49 -0600

    Add files via upload

---
+++ program4/src/program4/BFS_DFS.java
@@ -0,0 +1,80 @@
+package program4;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BFS_DFS {
+	public BufferedWriter out = null;
+	private Queue<Integer> queue;
+	private Stack<Integer> stack;
+	public BFS_DFS() throws IOException {
+		queue = new LinkedList<Integer>();
+		stack = new Stack<Integer>();
+		out = new BufferedWriter(new FileWriter("output.txt"));
+		
+	}
+	
+	
+	
+	
+	public void bfs(int adj_matrix[][], int source) throws IOException {
+		int nodeNum = adj_matrix[source].length;
+		int[] visited = new int[nodeNum + 1];
+		int i, element;
+		
+		visited[source] = 1;
+		queue.add(source);
+		
+		while(!queue.isEmpty()) {
+			element = queue.remove();
+			i = element;
+			out.write(i + "\t");
+			while(i<= nodeNum){
+				if(adj_matrix[element][i] == 1 && visited[i] == 0) {
+					queue.add(i);
+					visited[i] = 1;
+				}
+				i++;
+			}
+			out.newLine();
+		}
+		
+	}
+	
+	public void dfs(int adj_matrix[][], int source) throws IOException {
+		int nodeNum = adj_matrix[source].length -1;
+		int visited[] = new int[nodeNum +1];
+		int element = source;
+		int i = source;
+		out.write(element + "\t");
+		visited[source] = 1;
+		stack.push(source);
+		while(!stack.isEmpty()) {
+			element = stack.peek();
+			i = element;
+			while(i <= nodeNum) {
+				if(adj_matrix[element][i] == 1 && visited[i] == 0) {
+					stack.push(i);
+					visited[i] = 1;
+					element = i;
+					i = 1;
+					out.write(element + "\t");
+					continue;
+					
+				}
+				i++;
+			}
+			stack.pop();
+			out.newLine();
+		}
+		
+	}
+	
+}
---
+++ program4/src/program4/Driver.java
@@ -0,0 +1,61 @@
+package program4;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Driver {
+
+	public static void main(String[] args) throws IOException{
+		int source;
+		BufferedWriter out = new BufferedWriter(new FileWriter("output.txt"));
+		Scanner scan = new Scanner(System.in);
+		int[][] matrix = getMatrix();
+		BFS_DFS graph = new BFS_DFS();
+		System.out.println("Enter the source for your graph");
+		source = scan.nextInt();
+		out.write("The BFS traversal of the graph is: ");
+		out.newLine();
+		graph.bfs(matrix, source);
+		out.write("The DFS traversal of the graph is: ");
+		out.newLine();
+		graph.dfs(matrix, source);
+		
+	}
+
+	public static int[][] getMatrix(){
+		int[][] matrix = null;
+		int row = 0;
+		boolean start = true;
+		Scanner file = getFile();
+		while(file.hasNextLine()) {
+			String[] line = file.nextLine().split("\\s+");
+			if(start) {
+				matrix = new int[line.length][line.length];
+				start = false;
+			}
+			for(int i = 0; i< line.length; i++) {
+				int num = Integer.parseInt(line[i]);
+				matrix[row][i] = num;
+			}
+			row++;
+		}
+		return matrix;
+	}
+	
+	public static Scanner getFile() {
+		try {
+			@SuppressWarnings("resource")
+			Scanner file = new Scanner(new File("input.txt"));
+			return file;
+			
+		}catch(FileNotFoundException e) {
+			System.out.println("File not found");
+			System.exit(0);
+			return null;
+		}
+		
+	}
+}
